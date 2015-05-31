75902d778c989d52f40831e62bcfa3aac9a69900
RadioLemon <horsentp@gmail.com>
2015-05-31 14:39:46 -0500

    Implemented the Camera maths

--- netbeans-flowy/src/bropals/flowy/Camera.java
+++ netbeans-flowy/src/bropals/flowy/Camera.java
@@ -13,14 +13,36 @@ import java.awt.Point;
  */
 public class Camera {
     
+    // 1 = no zoom
+    private float zoom;
+    private int x; //Camera X position (World coordinates)
+    private int y; //Camera Y position (World coordinates)
+    
     public Point convertCanvasToWorld(Point p) {
-        
+        p.x = (int)(p.x*zoom) + x;
+        p.y = (int)(p.y*zoom) + y;
         return p;
     }
     
     public Point convertWorldToCanvas(Point p) {
-        
+        p.x = (int)((p.x - x)/zoom);
+        p.y = (int)((p.y - y)/zoom);
         return p;
     }
     
+    public int convertCanvasToWorldX(int x) {
+        return (int)(x*zoom) + this.x;
+    }
+    
+    public int convertCanvasToWorldY(int y) {
+        return (int)(y*zoom) + this.y;
+    }
+    
+    public int convertWorldToCanvasX(int x) {
+        return (int)((x - this.x)/zoom);
+    }
+    
+    public int convertWorldToCanvasY(int y) {
+        return (int)((y - this.y)/zoom);
+    }
 }
